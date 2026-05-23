3ee9e68d152ad3b9bd465be657c0af4ff82a1a81
Monisha <monishakhare37@gmail.com>
2026-05-23 19:27:45 +0530

    Removed exposed Firebase secrets

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvHueMYRVIpPrxysTnOkzamnTASA7g_oQ",
-  authDomain: "riveto-56a49.firebaseapp.com",
-  projectId: "riveto-56a49",
-  storageBucket: "riveto-56a49.firebasestorage.app",
-  messagingSenderId: "665369204411",
-  appId: "1:665369204411:web:1e3ca07099f82e30fc7b9e"
+  apiKey: "your_api_key",
+  authDomain: "your_project.firebaseapp.com",
+  projectId: "your_project_id",
+  storageBucket: "your_project.appspot.com",
+  messagingSenderId: "your_sender_id",
+  appId: "your_app_id",
 };
 
 const app = initializeApp(firebaseConfig);
